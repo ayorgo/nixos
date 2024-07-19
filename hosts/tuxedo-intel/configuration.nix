fc29a2dcb82232b264b205b5a9ecdae23019a4d0
Georgios Adzhygai <ayorgo@users.noreply.github.com>
2024-07-19 12:08:48 +0200

    Sound: tell PipeWire to not suspend sound device when inactive to avoid a 1 second delay when starting video or audio playback

--- hosts/tuxedo-intel/configuration.nix
+++ hosts/tuxedo-intel/configuration.nix
@@ -88,6 +88,54 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+
+    # https://wiki.archlinux.org/title/PipeWire#Noticeable_audio_delay_or_audible_pop/crack_when_starting_playback
+    wireplumber.configPackages = [
+      (pkgs.writeTextDir
+        "share/wireplumber/wireplumber.conf.d/51-disable-suspension.conf" ''
+          monitor.alsa.rules = [
+            {
+              matches = [
+                {
+                  # Matches all sources
+                  node.name = "~alsa_input.*"
+                },
+                {
+                  # Matches all sinks
+                  node.name = "~alsa_output.*"
+                }
+              ]
+              actions = {
+                update-props = {
+                  session.suspend-timeout-seconds = 0
+                }
+              }
+            }
+          ]
+          # bluetooth devices
+          monitor.bluez.rules = [
+            {
+              matches = [
+                {
+                  # Matches all sources
+                  node.name = "~bluez_input.*"
+                },
+                {
+                  # Matches all sinks
+                  node.name = "~bluez_output.*"
+                }
+              ]
+              actions = {
+                update-props = {
+                  session.suspend-timeout-seconds = 0
+                  dither.method = "wannamaker3",
+                  dither.noise = 2,
+                }
+              }
+            }
+          ]
+        '')
+    ];
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
 
