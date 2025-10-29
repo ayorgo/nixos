f80c2e4ce6e9bb7e379c7b6398d944e355a4f7ec
Georgios Adzhygai <ayorgo@users.noreply.github.com>
2025-10-29 10:22:29 +0100

    Librewolf: add Tab Session Manager extension

--- home/programs/librewolf/default.nix
+++ home/programs/librewolf/default.nix
@@ -47,6 +47,11 @@
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/british-english-dictionary-2/latest.xpi";
           installation_mode = "force_installed";
         };
+        # Tab sesson manager
+        "Tab-Session-Manager@sienori" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/tab-session-manager/latest.xpi";
+          installation_mode = "force_installed";
+        };
       };
     };
     profiles.default = {
