863ae97c65eaf91887b1c096e3a9792ff8013fbf
Georgios Adzhygai <ayorgo@users.noreply.github.com>
2025-11-22 16:38:34 +0000

    Darwin: fix the ssh warning

--- hosts/darwin/programs/git.nix
+++ hosts/darwin/programs/git.nix
@@ -9,8 +9,19 @@
 
     ssh = {
       enable = true;
-
-      # SSH client configuration
+      enableDefaultConfig = false;
+      matchBlocks."*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
       extraConfig = ''
         Host github.com
           IdentityFile ~/.ssh/github
