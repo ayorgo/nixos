3e0cbb9433c07e067f51944d49c30983180bb3b6
Georgios Adzhygai <ayorgo@users.noreply.github.com>
2025-12-19 23:26:25 +0000

    Darwin & Tuxedo merge: move flavours

--- hosts/tuxedo/flavours/dark/home.nix
+++ hosts/tuxedo/flavours/dark/home.nix
@@ -7,9 +7,9 @@
   programs.kitty.settings.active_tab_foreground = "#dcdfe4";  # `foreground` colour from OneHalfDark
   programs.kitty.settings.active_tab_background = "#282c34";  # `background` colour from OneHalfDark
   programs.kitty.settings.inactive_tab_background = "#282c34";  # `background` colour from OneHalfDark
-  programs.neovim.extraLuaConfig = (builtins.readFile ../../programs/neovim/init.lua + "\n" + "vim.cmd([[set background=dark]])");
+  programs.neovim.extraLuaConfig = (builtins.readFile ../../../../programs/neovim/init.lua + "\n" + "vim.cmd([[set background=dark]])");
   home.file."/home/ayorgo/.emacs.d/init.el" = {
-    text = "(setq my-theme-flavour \"dark\")" + "\n" + (builtins.readFile ../../programs/emacs/init.el);
+    text = "(setq my-theme-flavour \"dark\")" + "\n" + (builtins.readFile ../../../../programs/emacs/init.el);
   };
   dconf.settings = {
     "org/gnome/desktop/interface" = {
