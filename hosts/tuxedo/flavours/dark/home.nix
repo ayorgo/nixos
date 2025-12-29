a2ab282abf10e4b6312b51024ed72124da94168f
Georgios Adzhygai <ayorgo@users.noreply.github.com>
2025-12-29 20:41:33 +0100

    Darwin & Tuxedo merge: fix Tuxedo paths

--- hosts/tuxedo/flavours/dark/home.nix
+++ hosts/tuxedo/flavours/dark/home.nix
@@ -7,16 +7,16 @@
   programs.kitty.settings.active_tab_foreground = "#dcdfe4";  # `foreground` colour from OneHalfDark
   programs.kitty.settings.active_tab_background = "#282c34";  # `background` colour from OneHalfDark
   programs.kitty.settings.inactive_tab_background = "#282c34";  # `background` colour from OneHalfDark
-  programs.neovim.extraLuaConfig = (builtins.readFile ../../../../programs/neovim/init.lua + "\n" + "vim.cmd([[set background=dark]])");
+  programs.neovim.extraLuaConfig = (builtins.readFile ../../programs/neovim/init.lua + "\n" + "vim.cmd([[set background=dark]])");
   home.file."/home/ayorgo/.emacs.d/init.el" = {
-    text = "(setq my-theme-flavour \"dark\")" + "\n" + (builtins.readFile ../../../../programs/emacs/init.el);
+    text = "(setq my-theme-flavour \"dark\")" + "\n" + (builtins.readFile ../../programs/emacs/init.el);
   };
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
     };
     "org/gnome/desktop/background" = {
-      picture-uri = "file://" + ../../../wallpapers/nix-wallpaper-binary-black.png;
+      picture-uri = "file://" + ../../wallpapers/nix-wallpaper-binary-black.png;
     };
   };
   home.file.".config/nvim/bg".text = "dark";
