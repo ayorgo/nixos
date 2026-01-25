f7891c246b92d4485025b1e0aa7d05d904528cdc
Georgios Adzhygai <ayorgo@users.noreply.github.com>
2026-01-25 14:31:01 +0000

    Tuxedo: Emacs: migrate to the common application instance

--- programs/emacs/default.nix
+++ programs/emacs/default.nix
@@ -1,16 +1,23 @@
 { lib, pkgs, config, ... }:
 
 {
-  # The service doesn't seem to be working on MacOS
-  # services.emacs.enable = true;
+  # The service doesn't seem to be working on MacOS.
+  # It's not that useful on Linux either.
+  services.emacs.enable = false;
   programs.emacs = {
     enable = true;
-    package = pkgs.emacs30.override {
-      withSQLite3 = true;
-      withWebP = true;
-      withImageMagick = true;
-      withTreeSitter = true;
-    };
+    package = lib.mkMerge [
+      (lib.mkIf pkgs.stdenv.isDarwin (
+        pkgs.emacs30.override {
+          withSQLite3 = true;
+          withWebP = true;
+          withImageMagick = true;
+          withTreeSitter = true;
+      }))
+      (lib.mkIf pkgs.stdenv.isLinux
+        pkgs.emacs-pgtk
+      )
+    ];
   };
   home = {
     packages = with pkgs; [
@@ -18,10 +25,13 @@
       hunspellDicts.en-us-large
       hunspellDicts.en-gb-large
     ];
-    file = {
-      ".emacs.d/init.el" = {
-        source = ./init.el;
-      };
-    };
+    # On Tuxedo Linux `init.el` is copied by the flavours
+    file = lib.mkMerge [
+      (lib.mkIf pkgs.stdenv.isDarwin {
+          ".emacs.d/init.el" = {
+            source = ./init.el;
+          };
+      })
+    ];
   };
 }
