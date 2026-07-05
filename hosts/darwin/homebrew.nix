38a7097d2d341beb8c0e977181078fe1b70986bb
Georgios Adzhygai <ayorgo@users.noreply.github.com>
2026-07-05 13:01:11 +0100

    Darwin: add pgadmin4.

--- hosts/darwin/homebrew.nix
+++ hosts/darwin/homebrew.nix
@@ -6,6 +6,8 @@
   casks = [
     "qobuz"
     "karabiner-elements"
+    # The nixpkgs installation is failing for the time being.
+    "pgadmin4"
     # Requires all the applications in /Applications/Wacom\ Tablet.localized/
     # to be manually dragged and dropped to "Accessibility Access" System list
     "wacom-tablet"
