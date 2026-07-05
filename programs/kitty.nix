91e7032dfe45eff62427a3fa2760f6dd4f49a45d
Georgios Adzhygai <ayorgo@users.noreply.github.com>
2026-07-05 12:50:23 +0100

    Kitty: switch to previous_window shortcut.

--- programs/kitty.nix
+++ programs/kitty.nix
@@ -108,6 +108,7 @@
       "ctrl+k" = "neighboring_window up";
       "ctrl+h" = "neighboring_window left";
       "ctrl+l" = "neighboring_window right";
+      "shift+`" = "previous_window";
       "shift+tab" = "next_window";
 
       "alt+j" = "kitten relative_resize.py down  1";
