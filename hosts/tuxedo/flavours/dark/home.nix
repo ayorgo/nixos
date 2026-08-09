4f6b2bea6af8f8a2020417fb28c1215428f246fa
Georgios Adzhygai <ayorgo@users.noreply.github.com>
2026-08-09 20:58:31 +0200

    Tuxedo: fix dark/light mode switching for kitty and Neovim.

--- hosts/tuxedo/flavours/dark/home.nix
+++ hosts/tuxedo/flavours/dark/home.nix
@@ -4,10 +4,10 @@
   imports = [ ../../common.nix ];
 
   programs.kitty.themeFile = "OneHalfDark";
-  programs.kitty.settings.active_tab_foreground = "#dcdfe4";  # `foreground` colour from OneHalfDark
-  programs.kitty.settings.active_tab_background = "#282c34";  # `background` colour from OneHalfDark
-  programs.kitty.settings.inactive_tab_background = "#282c34";  # `background` colour from OneHalfDark
-  programs.neovim.initLua = (builtins.readFile ../../../../programs/neovim/init.lua + "\n" + "vim.cmd([[set background=dark]])");
+  programs.kitty.settings.active_tab_foreground = lib.mkForce "#dcdfe4";  # `foreground` colour from OneHalfDark
+  programs.kitty.settings.active_tab_background = lib.mkForce "#282c34";  # `background` colour from OneHalfDark
+  programs.kitty.settings.inactive_tab_background = lib.mkForce "#282c34";  # `background` colour from OneHalfDark
+  programs.neovim.initLua = ("\n" + "vim.cmd([[set background=dark]])");
   home.file."/home/ayorgo/.emacs.d/init.el" = {
     text = "(setq my-theme-flavour \"dark\")" + "\n" + (builtins.readFile ../../../../programs/emacs/init.el);
   };
